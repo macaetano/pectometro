dc796ffe1f146a7002b71aa368784333b8c3e399
Lucas Silveira <lucas.silveira.2010@hotmail.com>
2018-10-13 15:02:01 -0300

    Milagres acontecem

--- public/js/proposicoes.js
+++ public/js/proposicoes.js
@@ -1,27 +1,20 @@
+getUltimasProposicoes(5, () => { console.log(cb); })
+
 function exibirDadosPec(pecNum) {
     localStorage.setItem(`pec`, pecNum);
     window.location.href = 'file:///home/matheus/Documents/TIS/public/dadosPec.html';
 }
 
-// getUltimasProposicoes(5, function(response) { 
-//     //carousel.preencherUltimasProposicoes(response);
-//     carousel.addProposicao(response.dados[0], );
-//     console.log(response.dados);
-// });
-
-// function getUltimasProposicoes(quant, cb) {
-//     getIndexUltimaProposicao(function(ultima) {
-//         for (var i = ultima; i > ultima - quant; i--) {
-//             $.ajax({
-//                 "type": "GET",
-//                 "url": `https://dadosabertos.camara.leg.br/api/v2/proposicoes?siglaTipo=PEC&numero=${i}`,
-//                 "success": function(response) {
-//                     cb(response);
-//                 }
-//             });
-//         }
-//     });
-// }
+function getUltimasProposicoes(quant, cb) {
+    $.ajax({
+        "type": "GET",
+        "crossDomain": true,
+        "url": `https://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?sigla=PEC&numero=&ano=${(new Date()).getFullYear()}&datApresentacaoIni=&datApresentacaoFim=&parteNomeAutor=&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao=`,
+        "success": function(response) {
+            cb(response);
+        }
+    });
+}
 
 // function getIndexUltimaProposicao(cb, i=630) {
 //     $.ajax({
